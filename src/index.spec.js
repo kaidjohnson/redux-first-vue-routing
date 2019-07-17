6fffe7616d97f14e5721307ecaef18478a81de4e
Kai Johnson <kaidjohnson@gmail.com>
2019-07-17 15:03:09 -0400

    Include the plugin in unit testing of index.js

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,62 +1,67 @@
 import * as actionCreators from './action-creators.js';
-import * as reduxFirstVueRouter from './index.js';
+import * as reduxFirstVueRouting from './index.js';
 import middleware from './middleware.js';
+import plugin from './plugin.js';
 import reducer from './reducer.js';
 
 describe('redux-first-vue-router', () => {
 	it('exports a PUSH action type', () => {
-		expect(reduxFirstVueRouter.PUSH).toEqual('@@redux-first-vue-router/PUSH');
+		expect(reduxFirstVueRouting.PUSH).toEqual('@@redux-first-vue-router/PUSH');
 	});
 
 	it('exports a REPLACE action type', () => {
-		expect(reduxFirstVueRouter.REPLACE).toEqual('@@redux-first-vue-router/REPLACE');
+		expect(reduxFirstVueRouting.REPLACE).toEqual('@@redux-first-vue-router/REPLACE');
 	});
 
 	it('exports a GO action type', () => {
-		expect(reduxFirstVueRouter.GO).toEqual('@@redux-first-vue-router/GO');
+		expect(reduxFirstVueRouting.GO).toEqual('@@redux-first-vue-router/GO');
 	});
 
 	it('exports a BACK action type', () => {
-		expect(reduxFirstVueRouter.BACK).toEqual('@@redux-first-vue-router/BACK');
+		expect(reduxFirstVueRouting.BACK).toEqual('@@redux-first-vue-router/BACK');
 	});
 
 	it('exports a FORWARD action type', () => {
-		expect(reduxFirstVueRouter.FORWARD).toEqual('@@redux-first-vue-router/FORWARD');
+		expect(reduxFirstVueRouting.FORWARD).toEqual('@@redux-first-vue-router/FORWARD');
 	});
 
 	it('exports a LOCATION_CHANGE action type', () => {
-		expect(reduxFirstVueRouter.LOCATION_CHANGE).toEqual('@@redux-first-vue-router/LOCATION_CHANGE');
+		expect(reduxFirstVueRouting.LOCATION_CHANGE).toEqual('@@redux-first-vue-router/LOCATION_CHANGE');
 	});
 
 	it('exports a push action creator', () => {
-		expect(reduxFirstVueRouter.push).toBe(actionCreators.push);
+		expect(reduxFirstVueRouting.push).toBe(actionCreators.push);
 	});
 
 	it('exports a replace action creator', () => {
-		expect(reduxFirstVueRouter.replace).toBe(actionCreators.replace);
+		expect(reduxFirstVueRouting.replace).toBe(actionCreators.replace);
 	});
 
 	it('exports a go action creator', () => {
-		expect(reduxFirstVueRouter.go).toBe(actionCreators.go);
+		expect(reduxFirstVueRouting.go).toBe(actionCreators.go);
 	});
 
 	it('exports a back action creator', () => {
-		expect(reduxFirstVueRouter.back).toBe(actionCreators.back);
+		expect(reduxFirstVueRouting.back).toBe(actionCreators.back);
 	});
 
 	it('exports a forward action creator', () => {
-		expect(reduxFirstVueRouter.forward).toBe(actionCreators.forward);
+		expect(reduxFirstVueRouting.forward).toBe(actionCreators.forward);
 	});
 
 	it('exports a locationChange action creator', () => {
-		expect(reduxFirstVueRouter.locationChange).toBe(actionCreators.locationChange);
+		expect(reduxFirstVueRouting.locationChange).toBe(actionCreators.locationChange);
 	});
 
-	it('exports a routerMiddleware method', () => {
-		expect(reduxFirstVueRouter.middleware).toBe(middleware);
+	it('exports a middleware method', () => {
+		expect(reduxFirstVueRouting.middleware).toBe(middleware);
 	});
 
-	it('exports a routerReducer method', () => {
-		expect(reduxFirstVueRouter.reducer).toBe(reducer);
+	it('exports a reducer method', () => {
+		expect(reduxFirstVueRouting.reducer).toBe(reducer);
+	});
+
+	it('exports a Vue plugin by default', () => {
+		expect(reduxFirstVueRouting.default).toBe(plugin);
 	});
 });
